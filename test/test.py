ba0aef975a3956cb8b16b3cbddf747f31ec7299a
John <43251090+uhwang@users.noreply.github.com>
2026-02-05 14:30:36 -0500

    Add visualization for circle and ellipse coil geometry
    Added a script to visualize circle and ellipse coil geometry using matplotlib.

--- test/test.py
+++ test/test.py
@@ -1,1 +1,19 @@
+import matplotlib.pyplot as plt
+import coil_geom as cg
+
+coil = cg.CoilGeom()
+
+xc, yc = coil.circle_coil()
+xe, ye = coil.ellipse_coil()
+
+fig, axs = plt.subplots(2)
+fig.suptitle('Circle & Ellipse Coil Geometry')
+axs[0].plot(xc, yc)
+axs[1].plot(xe, ye)
+
+xmax = max(xc)
+axs[0].set_xlim(0,xmax)
+axs[0].set_aspect('equal')
+axs[1].set_xlim(0,xmax)
+plt.show()
 
