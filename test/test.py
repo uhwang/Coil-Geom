cdb959f92bda2b3cbf592a90450291ca73b64875
John <43251090+uhwang@users.noreply.github.com>
2026-02-06 22:58:46 -0500

    Modify subplots to include additional circle coil
    Added a third subplot for the circle coil with negative p_dist.

--- test/test.py
+++ test/test.py
@@ -2,18 +2,14 @@ import matplotlib.pyplot as plt
 import coil_geom as cg
 
 coil = cg.CoilGeom()
-
 xc, yc = coil.circle_coil()
 xe, ye = coil.ellipse_coil()
+xu, yu = coil.circle_coil(p_dist=-0.7)
 
-fig, axs = plt.subplots(2)
+fig, axs = plt.subplots(nrows=3, ncols=1, sharex=True, figsize=(8, 6))
 fig.suptitle('Circle & Ellipse Coil Geometry')
 axs[0].plot(xc, yc)
 axs[1].plot(xe, ye)
+axs[2].plot(xu, yu)
 
-xmax = max(xc)
-axs[0].set_xlim(0,xmax)
-axs[0].set_aspect('equal')
-axs[1].set_xlim(0,xmax)
 plt.show()
-
