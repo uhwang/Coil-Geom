e4b35b45135f2360f1ca79edc9a3be1eea85391d
John <43251090+uhwang@users.noreply.github.com>
2026-04-08 15:06:16 -0400

    Add files via upload

--- test/test.py
+++ test/test.py
@@ -1,15 +1,26 @@
 import matplotlib.pyplot as plt
 import coil_geom as cg
 
-coil = cg.CoilGeom()
-xc, yc = coil.circle_coil()
-xe, ye = coil.ellipse_coil()
-xu, yu = coil.circle_coil(p_dist=-0.7)
+c_c_up = cg.CircleCoil(p_dist=0.7)
+c_c_dn = cg.CircleCoil()
 
-fig, axs = plt.subplots(nrows=3, ncols=1, sharex=True, figsize=(8, 6))
-fig.suptitle('Circle & Ellipse Coil Geometry')
-axs[0].plot(xc, yc)
-axs[1].plot(xe, ye)
-axs[2].plot(xu, yu)
+c_e_up = cg.EllipseCoil(p_dist=0.4)
+c_e_dn = cg.EllipseCoil()
 
-plt.show()
+c_es_up = cg.EllipseCoilShape(p_dist=0.4)
+c_es_dn = cg.EllipseCoilShape()
+
+c_ec_up = cg.EllipseCoilCurvature(p_dist=0.4)
+c_ec_dn = cg.EllipseCoilCurvature()
+
+cg.save_ppt(c_c_up , "c_c_up.pptx" )
+cg.save_ppt(c_c_dn , "c_c_dn.pptx" )
+
+cg.save_ppt(c_e_up , "c_e_up.pptx" )
+cg.save_ppt(c_e_dn , "c_e_dn.pptx" )
+
+cg.save_ppt(c_es_up, "c_es_up.pptx")
+cg.save_ppt(c_es_dn, "c_es_dn.pptx")
+
+cg.save_ppt(c_ec_up, "c_ec_up.pptx")
+cg.save_ppt(c_ec_dn, "c_ec_dn.pptx")
