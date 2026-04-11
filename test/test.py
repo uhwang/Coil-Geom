d1462f6df80021e1b230db4a85df28583eedf43c
John <43251090+uhwang@users.noreply.github.com>
2026-04-11 16:54:51 -0400

    Add files via upload

--- test/test.py
+++ test/test.py
@@ -1,38 +1,32 @@
 import matplotlib.pyplot as plt
 import coil_geom as cg
 
-c_c_up = cg.CircleCoil(p_dist=0.7)
-c_c_dn = cg.CircleCoil()
+c_c_up = cg.CircleCoil(p_dist=0.7, ncoil=5)
+c_c_dn = cg.CircleCoil(ncoil=5)
 
-c_e_up = cg.EllipseCoil(p_dist=0.4)
-c_e_dn = cg.EllipseCoil()
+c_e_up = cg.EllipseCoil(p_dist=0.4, ncoil=5)
+c_e_dn = cg.EllipseCoil(ncoil=5)
 
-c_es_up = cg.EllipseCoilShape(p_dist=0.4)
-c_es_dn = cg.EllipseCoilShape()
+c_es_up = cg.EllipseCoilShape(p_dist=0.4, target=0.8, ncoil=5)
+c_es_dn = cg.EllipseCoilShape(ncoil=5)
 
-c_ec_up = cg.EllipseCoilCurvature(p_dist=0.4)
-c_ec_dn = cg.EllipseCoilCurvature()
+c_ec_up = cg.EllipseCoilCurvature(p_dist=0.4, ncoil=5)
+c_ec_dn = cg.EllipseCoilCurvature(ncoil=5)
 
 cg.save_ppt(c_c_up , "c_c_up.pptx" )
 cg.save_ppt(c_c_dn , "c_c_dn.pptx" )
-
 cg.save_ppt(c_e_up , "c_e_up.pptx" )
 cg.save_ppt(c_e_dn , "c_e_dn.pptx" )
-
-cg.save_ppt(c_es_up, "c_es_up.pptx")
-cg.save_ppt(c_es_dn, "c_es_dn.pptx")
-
+cg.save_ppt(c_es_up, "c_es_up.pptx", debug=True)
+cg.save_ppt(c_es_dn, "c_es_dn.pptx", debug=True)
 cg.save_ppt(c_ec_up, "c_ec_up.pptx")
 cg.save_ppt(c_ec_dn, "c_ec_dn.pptx")
 
 cg.save_svg(c_c_up , "c_c_up.svg" )
 cg.save_svg(c_c_dn , "c_c_dn.svg" )
-
 cg.save_svg(c_e_up , "c_e_up.svg" )
 cg.save_svg(c_e_dn , "c_e_dn.svg" )
-
-cg.save_svg(c_es_up, "c_es_up.svg")
-cg.save_svg(c_es_dn, "c_es_dn.svg")
-
+cg.save_svg(c_es_up, "c_es_up.svg", debug=True)
+cg.save_svg(c_es_dn, "c_es_dn.svg", debug=True)
 cg.save_svg(c_ec_up, "c_ec_up.svg")
 cg.save_svg(c_ec_dn, "c_ec_dn.svg")
