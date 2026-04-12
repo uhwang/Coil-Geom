26d30ae18b579738987d3e6a051ee573e314c9b7
John <43251090+uhwang@users.noreply.github.com>
2026-04-12 15:59:02 -0400

    Update version to 0.2.10 and add changelog

--- coil_geom/__init__.py
+++ coil_geom/__init__.py
@@ -1,4 +1,12 @@
-__version__ = "0.2.8"
+'''
+    Ver. 0.2.6   PPTX export added
+    Ver. 0.2.7   SVG export added
+    Ver. 0.2.8   Debug plot added
+    Ver. 0.2.9   PDF export added
+    Ver. 0.2.10  Coord conversion fixed
+'''
+
+__version__ = "0.2.10"
 
 from .coil_geom import *
 from .coil_gen import *
