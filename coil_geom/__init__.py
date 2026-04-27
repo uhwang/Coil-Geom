82a583d44f624df6a3e17dfab0209e1a9a9fd2fb
John <43251090+uhwang@users.noreply.github.com>
2026-04-26 23:12:24 -0400

    Add files via upload

--- coil_geom/__init__.py
+++ coil_geom/__init__.py
@@ -4,12 +4,15 @@
     Ver. 0.2.8   Debug plot added
     Ver. 0.2.9   PDF export added
     Ver. 0.2.10  Coord conversion fixed
+    Ver. 0.2.11  Affine Transformation added
+    Ver. 0.2.18  flipud(), fliplr(), rotate()
 '''
 
-__version__ = "0.2.13"
+__version__ = "0.2.18"
 
 from .coil_geom import *
 from .coil_gen import *
 from .coil_print import *
 from .coil_util import *
 from .coil_export import *
+from .coil_3phase import *
