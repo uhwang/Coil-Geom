92439512a564feb731215a1a348e56496c6981ea
John <43251090+uhwang@users.noreply.github.com>
2026-04-28 19:04:56 -0400

    Add files via upload

--- coil_geom/coil_3phase.py
+++ coil_geom/coil_3phase.py
@@ -22,6 +22,9 @@ else:
 #   ecs: Ellipse Coil Shape
 
 coil_t_list = ["cc", "ec", "ecs", "ecc"]
+phase_t_list = ["delta", "y"]
+phase_t_delta = phase_t_list[0]
+phase_t_y = phase_t_list[1]
     
 def three_delta(coil, lead_l, lead_r):
 
@@ -70,65 +73,79 @@ def create_coil(c_t, p_dist, ncoil):
         coil = cg.EllipseCoilCurvature(p_dist=p_dist, ncoil=ncoil)
  
     return coil
-    
-def save_3phase_delta(fname, p_dist=0.4, ncoil=5, lead_l=4, lead_r=4, lcol='b', lthk=0.01, coil_t="ecc"):
 
+def create_3phase(coil_t, p_dist, ncoil, lead_l, lead_r, phase_t):
     c_t = coil_t.lower()
     if c_t not in coil_t_list:
-        return 
+        raise ValueError(f"Error(create_3phase): invalid coil type->{c_t}")
     
     coil = create_coil(c_t, p_dist, ncoil)
-    xs, ys = three_delta(coil, lead_l, lead_r)
+    if phase_t == phase_t_delta: 
+        xs, ys = three_delta(coil, lead_l, lead_r)
+    elif phase_t == phase_t_y: 
+        xs, ys = three_y(coil, lead_l, lead_r)
+    else:
+        raise ValueError(f"Error(create_3phase): invalid phase type->{phase_t}")
+        
+    return xs, ys
     
+def save_3phase_delta(fname, p_dist=0.4, ncoil=5, lead_l=4, lead_r=4, lcol='b', lthk=0.01, coil_t="ecc"):
+    try:
+        xs, ys = create_3phase(coil_t, p_dist, ncoil, lead_l, lead_r, phase_t_delta)
+    except Exception as e:
+        print(f"Error(save_3phase_delta->create_3phase): {e}")
+        return
+        
     dev = cxp.DevicePPT(fname, xs, ys)
     for x_, y_ in zip(xs, ys):
         dev.polyline(dev.xs_(x_), dev.ys_(y_), lcol, lthk)
 
-    dev.close()        
+    dev.close()  
+
+    return xs, ys    
 
 def plot_3phase_delta(p_dist=0.4, ncoil=5, lead_l=5, lead_r=5, lcol='b', lthk=0.01, coil_t="ecc"):
     import matplotlib.pyplot as plt
     
-    c_t = coil_t.lower()
-    if c_t not in coil_t_list:
-        return 
-    
-    coil = create_coil(c_t, p_dist, ncoil)
-    xs, ys = three_delta(coil, lead_l, lead_r)
+    try:
+        xs, ys = create_3phase(coil_t, p_dist, ncoil, lead_l, lead_r, phase_t_y)
+    except Exception as e:
+        print(f"Error(plot_3phase_delta->create_3phase): {e}")
+        return
         
-    plt.plot(xs[0], ys[0])
-    plt.plot(xs[1], ys[1])
-    plt.plot(xs[2], ys[2])
+    plt.plot(xs[0], ys[0], color=lcol)
+    plt.plot(xs[1], ys[1], color=lcol)
+    plt.plot(xs[2], ys[2], color=lcol)
     plt.axis('equal')
     plt.show()
         
 def save_3phase_y(fname, p_dist=0.4, ncoil=5, lead_l=6, lead_r=6, lcol='b', lthk=0.01, coil_t="ecc"):
 
-    c_t = coil_t.lower()
-    if c_t not in coil_t_list:
-        return 
-    
-    coil = create_coil(c_t, p_dist, ncoil)
-    xs, ys = three_y(coil, lead_l, lead_r)
+    try:
+        xs, ys = create_3phase(coil_t, p_dist, ncoil, lead_l, lead_r, phase_t_y)
+    except Exception as e:
+        print(f"Error(save_3phase_y->create_3phase): {e}")
+        return
     
     dev = cxp.DevicePPT(fname, xs, ys)
     for x_, y_ in zip(xs, ys):
         dev.polyline(dev.xs_(x_), dev.ys_(y_), lcol, lthk)
 
-    dev.close()        
+    dev.close()
+
+    return xs, ys
 
 def plot_3phase_y(p_dist=0.4, ncoil=5, lead_l=5, lead_r=5, lcol='b', lthk=0.01, coil_t="ecc"):
     import matplotlib.pyplot as plt
     
-    c_t = coil_t.lower()
-    if c_t not in coil_t_list:
-        return 
-    
-    coil = create_coil(c_t, p_dist, ncoil)
-    xs, ys = three_y(coil, lead_l, lead_r)
+    try:
+        xs, ys = create_3phase(coil_t, p_dist, ncoil, lead_l, lead_r, phase_t_y)
+    except Exception as e:
+        print(f"Error(plot_3phase_y->create_3phase): {e}")
+        return
         
-    plt.plot(xs[0], ys[0])
-    plt.plot(xs[1], ys[1])
-    plt.plot(xs[2], ys[2])
+    plt.plot(xs[0], ys[0], color=lcol)
+    plt.plot(xs[1], ys[1], color=lcol)
+    plt.plot(xs[2], ys[2], color=lcol)
     plt.axis('equal')
     plt.show()
