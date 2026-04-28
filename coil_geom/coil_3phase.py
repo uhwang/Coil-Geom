ebf8dbcfbd76338cf242c33a90988e59b1e83e1c
John <43251090+uhwang@users.noreply.github.com>
2026-04-27 21:54:06 -0400

    Add files via upload

--- coil_geom/coil_3phase.py
+++ coil_geom/coil_3phase.py
@@ -40,7 +40,24 @@ def three_delta(coil, lead_l, lead_r):
     ys.append(c3.y)
         
     return xs, ys
- 
+
+def three_y(coil, lead_l, lead_r):
+
+    xs, ys = [], []
+    c = coil.create_geom(False, lead_l, lead_r)
+    c1 = c.rotate(90, axis=0)
+    xs.append(c1.x)
+    ys.append(c1.y)
+    
+    c2 = c1.rotate(120, axis=0)
+    xs.append(c2.x)
+    ys.append(c2.y)
+    
+    c3 = c2.rotate(120, axis=0)
+    xs.append(c3.x)
+    ys.append(c3.y)        
+    return xs, ys
+    
 def create_coil(c_t, p_dist, ncoil):
         
     if c_t == coil_t_list[0]:
@@ -69,7 +86,7 @@ def save_3phase_delta(fname, p_dist=0.4, ncoil=5, lead_l=4, lead_r=4, lcol='b', 
 
     dev.close()        
 
-def plot_3phase_delta(p_dist=0.4, ncoil=5, lead_l=4, lead_r=4, lcol='b', lthk=0.01, coil_t="ecc"):
+def plot_3phase_delta(p_dist=0.4, ncoil=5, lead_l=5, lead_r=5, lcol='b', lthk=0.01, coil_t="ecc"):
     import matplotlib.pyplot as plt
     
     c_t = coil_t.lower()
@@ -82,7 +99,36 @@ def plot_3phase_delta(p_dist=0.4, ncoil=5, lead_l=4, lead_r=4, lcol='b', lthk=0.
     plt.plot(xs[0], ys[0])
     plt.plot(xs[1], ys[1])
     plt.plot(xs[2], ys[2])
+    plt.axis('equal')
     plt.show()
         
-def three_y(cg):
-    pass
+def save_3phase_y(fname, p_dist=0.4, ncoil=5, lead_l=6, lead_r=6, lcol='b', lthk=0.01, coil_t="ecc"):
+
+    c_t = coil_t.lower()
+    if c_t not in coil_t_list:
+        return 
+    
+    coil = create_coil(c_t, p_dist, ncoil)
+    xs, ys = three_y(coil, lead_l, lead_r)
+    
+    dev = cxp.DevicePPT(fname, xs, ys)
+    for x_, y_ in zip(xs, ys):
+        dev.polyline(dev.xs_(x_), dev.ys_(y_), lcol, lthk)
+
+    dev.close()        
+
+def plot_3phase_y(p_dist=0.4, ncoil=5, lead_l=5, lead_r=5, lcol='b', lthk=0.01, coil_t="ecc"):
+    import matplotlib.pyplot as plt
+    
+    c_t = coil_t.lower()
+    if c_t not in coil_t_list:
+        return 
+    
+    coil = create_coil(c_t, p_dist, ncoil)
+    xs, ys = three_y(coil, lead_l, lead_r)
+        
+    plt.plot(xs[0], ys[0])
+    plt.plot(xs[1], ys[1])
+    plt.plot(xs[2], ys[2])
+    plt.axis('equal')
+    plt.show()
